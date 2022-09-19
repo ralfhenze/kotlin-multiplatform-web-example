bed4ac6f54e58ab693d27dbc1189e8471713007c
Ralf Henze <mail@ralfhenze.dev>
2022-09-19 15:26:13 +0200

    Add simple Vue.js frontend with task form

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TaskForm from "@/components/TaskForm.vue";
+</script>
+
+<template>
+    <h1>Task List</h1>
+    <p>
+        This is a very simple web application to demonstrate code sharing between backend
+        and frontend with Kotlin Multiplatform.
+    </p>
+    <h2>Create a new task</h2>
+    <TaskForm />
+</template>
---
+++ frontend/src/components/TaskForm.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <form>
+        <div>
+            <label>State</label>
+            <select>
+                <option>TODO</option>
+                <option>IN PROGRESS</option>
+                <option>DONE</option>
+            </select>
+        </div>
+
+        <div>
+            <label>Description</label>
+            <textarea></textarea>
+        </div>
+
+        <div>
+            <input type="submit" value="Create">
+        </div>
+    </form>
+</template>
